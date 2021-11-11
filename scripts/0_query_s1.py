8652291fd5093b7f6df719ea54c8edd68299884c
loreabad6 <lore.abad6@gmail.com>
2021-11-11 14:38:04 +0100

    Update query script, works via ASF, gets temporal, geographical and baseline

--- scripts/0_query_s1.py
+++ scripts/0_query_s1.py
@@ -17,10 +17,14 @@ Update: November 2021
 import asf_search as asf
 from dotenv import load_dotenv
 import logging
+import numpy as np
 import os
 import pandas as pd
 from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
+import requests
 
+pd.options.mode.chained_assignment = None  # default='warn'
+# https://stackoverflow.com/questions/20625582/how-to-deal-with-settingwithcopywarning-in-pandas
 logging.basicConfig(format='%(message)s', level='INFO')
 
 # VARIABLES SET START------
@@ -31,15 +35,18 @@ os.chdir('home/')
 
 # Set the working directory and search queries
 download_folder = r"data/s1"
+query_result_file = "s1_scenes.csv"
 
 # Set date range.
-# NB: Older images may be "Offline".
-# See Sentinelsat documentation on how to request offline products
-date_start = '2019-06-01'
-date_end = '2019-10-30'
+date_start = '2020-06-10'
+date_end = '2020-06-30'
 
 # GeoJSON file of study area. Any images overlapping this will be downloaded
 aoi = r"data/aoi/Alta.geojson"
+
+# Set maximum temporal baseline and minimum perpendicular baseline
+btempth = 10
+bperpth = 100
 
 # Repository to query, can be sentinelhub or asf
 repo = 'asf'
@@ -48,6 +55,7 @@ repo = 'asf'
 # Setup params
 dates = '[' + date_start + 'T00:00:00.000Z TO ' + date_end + 'T00:00:00.000Z]'
 footprint = geojson_to_wkt(read_geojson(aoi))
+tempfile1 = 'tmpgeo.csv'
 
 # Connect to API and search
 if repo == 'sentinelhub':
@@ -79,9 +87,8 @@ if repo == 'sentinelhub':
     products_df = api.to_dataframe(products)
 
     # Write to CSV file
-    file_name = os.path.join(download_folder, "sentinelhub_images.csv")
+    file_name = os.path.join(download_folder, tempfile)
     products_df.to_csv(file_name, index=False)
-    print("CSV file with images to be processed has been written to " + file_name)
 elif repo == "asf":
     products = asf.geo_search(platform=[asf.PLATFORM.SENTINEL1],
                               intersectsWith=footprint,
@@ -92,8 +99,60 @@ elif repo == "asf":
     products_df = pd.DataFrame([p.properties for p in products])
 
     # Write to CSV file
-    file_name = os.path.join(download_folder, "asf_images.csv")
+    file_name = os.path.join(download_folder, tempfile)
     products_df.to_csv(file_name, index=False)
-    print("CSV file with images to be processed has been written to " + file_name)
 else:
     print("Repository not supported.")
+
+# Create empty list to hold the results
+candidates = []
+
+# Read scene IDs
+# Get ids for filtered images
+geo_prod = pd.read_csv(os.path.join(download_folder, tempfile))
+geo_ids = geo_prod['fileID'].map(lambda fileID: str.replace(fileID, '-SLC', '')).tolist()
+
+# Loop over geo_ids to get matching scenes with desired temporal and perpendicular baselines
+tempfile2 = 'tmpbaseline.csv'
+for i in range(0, len(geo_ids)):
+    order_url = "https://api.daac.asf.alaska.edu/services/search/baseline?reference="
+    scene_id = geo_ids[i]
+    output_type = "&output=csv"
+    url = order_url + scene_id + output_type
+    response = requests.post(url)
+
+    # Write to .CSV
+    baseline_file = os.path.join(download_folder, tempfile2)
+    f = open(baseline_file, "w")
+    f.write(response.text)
+    f.close()
+
+    # Read in CSV file
+    baseline = pd.read_csv(baseline_file).replace(to_replace='None', value=np.nan)
+    baseline = baseline[(baseline.TemporalBaseline.notnull()) &
+                        (baseline.PerpendicularBaseline.notnull())]
+    baseline[['TemporalBaseline', 'PerpendicularBaseline']] = \
+        baseline[['TemporalBaseline', 'PerpendicularBaseline']].apply(pd.to_numeric)
+
+    baseline_filter = baseline[(abs(baseline['TemporalBaseline']) <= btempth) &
+                               (abs(baseline['PerpendicularBaseline']) >= bperpth)]
+    baseline_df = baseline_filter[
+        ['Granule Name', 'Path Number', 'Ascending or Descending?', 'TemporalBaseline', 'PerpendicularBaseline']]
+    baseline_df.rename(columns={'Granule Name': 'MatchID', 'Path Number': 'Orbit', 'Ascending or Descending?': 'Pass'},
+                       inplace=True)
+    baseline_df.insert(0, 'ReferenceID', scene_id, True)
+
+    candidates.append(baseline_df)
+
+# Merge all dataframes
+candidates_df = pd.concat(candidates)
+
+# Check if matched ids are also intersecting with the AOI and dates set
+candidates_df['inAOI'] = candidates_df['MatchID'].isin(geo_ids)
+
+# Write to CSV file and remove temporal files
+file_name = os.path.join(download_folder, query_result_file)
+candidates_df.to_csv(file_name, index=False)
+os.remove(os.path.join(download_folder, tempfile1))
+os.remove(os.path.join(download_folder, tempfile2))
+print("CSV file with images to be processed has been written to " + file_name)
